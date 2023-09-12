d1e3795e4b8a368812d56e2df1f9cf04e88dbeaa
peppegerace <peppegerace47@gmail.com>
2023-09-12 16:50:08 +0200

    [add] mail program

---
+++ mail/js/script.js
@@ -0,0 +1,50 @@
+//  array contenente la lista di email
+const listaMail = [
+  'AldoAquino@boolean.it',
+  'MassimoAzzini@boolean.it',
+  'VahidBan@boolean.it',
+  'GiacomoBechini@boolean.it',
+  'ThomasBortolotti@boolean.it',
+  'CarmeloCammalleri@boolean.it',
+  'AlessandroCingolani@boolean.it',
+  'Andreea@boolean.it',
+  'DavideCorrea@boolean.it',
+  'AlessandroDAniello@boolean.it',
+  'ArgelAllanDelaCruz@boolean.it',
+  'ManueleDiGregorio@boolean.it',
+  'EmanueleDiLeo@boolean.it',
+  'EddyFabbricatore@boolean.it',
+  'AntonioFaddanno@boolean.it',
+  'LeonardoFlammini@boolean.it',
+  'VeronicaFontana@boolean.it',
+  'GiuseppeGerace@boolean.it',
+  'VincenzoImmordino@boolean.it',
+  'VincenzoLiCalzi@boolean.it',
+  'PietroLupi@boolean.it',
+  'AlessandroMatricardi@boolean.it',
+  'AlessiaPatanè@boolean.it',
+  'SilviaPetrino@boolean.it',
+  'EstherPinos@boolean.it',
+  'AlessandroPirisinu@boolean.it',
+  'GianniMarkQuagliani@boolean.it',
+  'FrancescoRussotto@boolean.it',
+  'FranciscoSánchez@boolean.it',
+  'ManvinderSingh@boolean.it',
+  'SalvatoreDarioTorrisi@boolean.it',
+  'ErikVella@boolean.it',
+  'AsiaViscardi@boolean.it'
+];
+console.log(listaMail)
+
+const inviaMail = document.getElementById('invia')
+
+inviaMail.addEventListener('click', function(){
+  const mailInserita = document.getElementById('email').value;
+  console.log(mailInserita)
+
+  if (listaMail.includes(mailInserita)) {
+    document.querySelector('.output').innerHTML = 'Puoi continuare la navigazione su questa pagina'
+  } else {
+    document.querySelector('.output').innerHTML = 'Accesso negato'
+  }
+})
